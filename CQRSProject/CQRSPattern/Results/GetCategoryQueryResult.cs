fe2439aa7de6c43ee1145139c4da58ed31b07fd7
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-25 20:41:29 +0300

    product ve category result sınıfları oluşturuldu

---
+++ CQRSProject/CQRSPattern/Results/GetCategoryQueryResult.cs
@@ -0,0 +1,8 @@
+﻿namespace CQRSProject.CQRSPattern.Results
+{
+    public class GetCategoryQueryResult
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ CQRSProject/CQRSPattern/Results/GetProductQueryResult.cs
@@ -0,0 +1,10 @@
+﻿namespace CQRSProject.CQRSPattern.Results
+{
+    public class GetProductQueryResult
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+    }
+}
