93f70ab3736474d04e891417ae206a99b50ae4f7
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-25 20:46:45 +0300

    handlers ve result sınıfları oluşturuldu

---
+++ CQRSProject/CQRSPattern/Handlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using CQRSProject.CQRSPattern.Results;
+using CQRSProject.DAL.Context;
+
+namespace CQRSProject.CQRSPattern.Handlers
+{
+    public class GetCategoryQueryHandler
+    {
+        private readonly SaleContext _context;
+
+        public GetCategoryQueryHandler(SaleContext context)
+        {
+            _context = context;
+        }
+        public List<GetCategoryQueryResult> Handle()
+        {
+            var values = _context.Categories.Select(x => new GetCategoryQueryResult
+            {
+                CategoryID = x.CategoryID,
+                CategoryName = x.CategoryName,
+            }).ToList();
+            return values;
+        }
+    }
+}
--- CQRSProject/CQRSPattern/Results/GetProductQueryResult.cs
+++ CQRSProject/CQRSPattern/Results/GetProductQueryResult.cs
@@ -6,5 +6,6 @@
         public string ProductName { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
+        public int CategoryID { get; set; }
     }
 }
