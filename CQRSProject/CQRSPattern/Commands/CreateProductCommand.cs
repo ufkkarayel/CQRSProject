36de8db80936c880cebc5e9a04716e281325cfc0
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-25 21:36:30 +0300

    handler eklendi

--- CQRSProject/CQRSPattern/Commands/CreateProductCommand.cs
+++ CQRSProject/CQRSPattern/Commands/CreateProductCommand.cs
@@ -2,5 +2,10 @@
 {
     public class CreateProductCommand
     {
+        
+        public string ProductName { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryID { get; set; }
     }
 }
