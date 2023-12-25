6d0333cd2831cd547512c34a5c73c5a30b8a4170
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-25 20:28:45 +0300

    Add project files.

---
+++ CQRSProject/DAL/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace CQRSProject.DAL.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
