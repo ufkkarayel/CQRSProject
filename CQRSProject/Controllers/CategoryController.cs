39797fe20d1ddab4beec5396b78195340075a884
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-25 21:42:37 +0300

    controllera ekleme yapıldı

--- CQRSProject/Controllers/CategoryController.cs
+++ CQRSProject/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using CQRSProject.CQRSPattern.Handlers;
+﻿using CQRSProject.CQRSPattern.Commands;
+using CQRSProject.CQRSPattern.Handlers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CQRSProject.Controllers
@@ -6,16 +7,29 @@ namespace CQRSProject.Controllers
     public class CategoryController : Controller
     {
         private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
 
-        public CategoryController(GetCategoryQueryHandler getCategoryQueryHandler)
+        public CategoryController(GetCategoryQueryHandler getCategoryQueryHandler, CreateCategoryCommandHandler createCategoryCommandHandler)
         {
             _getCategoryQueryHandler = getCategoryQueryHandler;
+            _createCategoryCommandHandler = createCategoryCommandHandler;
         }
 
         public IActionResult Index()
         {
             var values = _getCategoryQueryHandler.Handle();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryCommand command)
+        {
+            _createCategoryCommandHandler.Handle(command);
+            return RedirectToAction("Index");
         }
     }
 }
