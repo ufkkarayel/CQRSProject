0d702193925bcbb51afec2488f46e5608bec4bf7
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-26 21:25:01 +0300

    ..

---
+++ CQRSProject/MediatorDesignPattern/Handlers/GetAllCustomerQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using CQRSProject.MediatorDesignPattern.Queries;
+using CQRSProject.MediatorDesignPattern.Results;
+using MediatR;
+
+namespace CQRSProject.MediatorDesignPattern.Handlers
+{
+    public class GetAllCustomerQueryHandler : IRequestHandler<GetAllCustomerQuery, List<GetAllCustormerQueryResult>>
+    {
+        public Task<List<GetAllCustormerQueryResult>> Handle(GetAllCustomerQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
