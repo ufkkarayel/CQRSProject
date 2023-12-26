9370c3f845e1a7170b4c4e8f8a6b5d2196aacb4a
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-26 21:45:11 +0300

    ..

---
+++ CQRSProject/MediatorDesignPattern/Commands/CreateCustomerCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace CQRSProject.MediatorDesignPattern.Commands
+{
+    public class CreateCustomerCommand : IRequest
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ CQRSProject/MediatorDesignPattern/Handlers/CreateCustomerCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CQRSProject.DAL.Context;
+using CQRSProject.DAL.Entities;
+using CQRSProject.MediatorDesignPattern.Commands;
+using MediatR;
+
+namespace CQRSProject.MediatorDesignPattern.Handlers
+{
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand>
+    {
+        private readonly SaleContext _context;
+
+        public CreateCustomerCommandHandler(SaleContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            _context.Customers.Add(new Customer
+            {
+                Department = request.Department,
+                Name = request.Name,
+                Surname = request.Surname,
+            });
+            await _context.SaveChangesAsync();
+    }
+}
