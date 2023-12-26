684f5b975b8b1f062cdfb15c8391c52b3a0e4041
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-26 21:17:18 +0300

    mediator dp eklendi

--- CQRSProject/DAL/Context/SaleContext.cs
+++ CQRSProject/DAL/Context/SaleContext.cs
@@ -11,5 +11,6 @@ namespace CQRSProject.DAL.Context
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
     }
 }
---
+++ CQRSProject/DAL/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace CQRSProject.DAL.Entities
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+    }
+}
