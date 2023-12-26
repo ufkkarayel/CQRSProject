c716c8ecacc8cb444df4cc58a0d5685732042dce
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-26 20:22:38 +0300

    update category oluşturuldu

---
+++ CQRSProject/CQRSPattern/Commands/UpdateCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace CQRSProject.CQRSPattern.Commands
+{
+    public class UpdateCategoryCommand
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
