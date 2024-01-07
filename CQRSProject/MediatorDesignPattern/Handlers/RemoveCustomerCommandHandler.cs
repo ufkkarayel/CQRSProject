5ee7d6494b744f5fa9b00326749c2a3315bdc885
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-07 15:18:38 +0300

    removecustomercommand handler eklendi

---
+++ CQRSProject/MediatorDesignPattern/Handlers/RemoveCustomerCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CQRSProject.CQRSPattern.Commands;
+using CQRSProject.DAL.Context;
+using CQRSProject.MediatorDesignPattern.Commands;
+using MediatR;
+using NuGet.Protocol.Plugins;
+
+namespace CQRSProject.MediatorDesignPattern.Handlers
+{
+    public class RemoveCustomerCommandHandler : IRequestHandler<RemoveCustomerCommand>
+    {
+        private readonly SaleContext _context;
+
+        public RemoveCustomerCommandHandler(SaleContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(RemoveCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var values = _context.Customers.Find(request.Id);
+            _context.Customers.Remove(values);
+            await _context.SaveChangesAsync(); 
+        }
+    }
+}
