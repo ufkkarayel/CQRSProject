3222cb53573592a6dd12d4829b1bf71cc8a823e7
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-07 18:15:33 +0300

    ..

---
+++ CQRSProject/MediatorDesignPattern/Handlers/UpdateCustomerCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CQRSProject.DAL.Context;
+using CQRSProject.MediatorDesignPattern.Commands;
+using MediatR;
+
+namespace CQRSProject.MediatorDesignPattern.Handlers
+{
+    public class UpdateCustomerCommandHandler : IRequestHandler<UpdateCustomerCommand>
+    {
+        private readonly SaleContext _context;
+
+        public UpdateCustomerCommandHandler(SaleContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var values = _context.Customers.Find(request.CustomerID);
+            values.Surname= request.Surname;
+            values.Department= request.Department;
+            values.Name= request.Name;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
